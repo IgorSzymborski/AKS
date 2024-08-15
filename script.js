9f455976b1486a0510778ffcf8ca291c13a4026e
Igor <igor.szymborski98@gmail.com>
2024-08-15 23:51:07 +0200

    update link

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ const pricingBox = document.querySelector(".pricing-box");
 const pricingCloseBtn = document.querySelector(".pricingCloseBtn");
 const overflowH = document.querySelector(".overflow-hidden");
 
-console.log(pricingLink);
-
 const togglePopup = (button) => {
   popups.forEach((popup) => {
     if (popup.getAttribute("data-popup") === button.getAttribute("data-btn")) {
@@ -46,25 +44,29 @@ closeMenuBtn.forEach((button) => {
     contactBox.classList.toggle("hiddenn");
     hamburgerButton.style.position = "static";
     hamburgerButton.classList.remove("active");
-
-    document.body.classList.remove(".overflowH");
     document.body.style.overflow = "";
+
+    console.log(button.classList.contains("pricingLink"));
+
+    if (button.classList.contains("pricingLink")) {
+      document.body.style.overflow = "hidden";
+    }
+
+    // document.body.classList.remove(".overflowH");
   });
 });
 
-console.log(pricingLink);
-console.log(navLink);
-
 pricingLink.addEventListener("click", function () {
-  console.log("test");
   pricingBox.classList.remove("hidden");
 
-  //dodanme
-  document.body.classList.add("overflowH");
+  document.body.style.overflow = "hidden";
+
+  console.log(document.body.style.overflow);
 });
 
 pricingCloseBtn.addEventListener("click", () => {
   pricingBox.classList.toggle("hidden");
+  document.body.style.overflow = "";
 });
 
 const map = L.map("map").setView([52.39880766106131, 20.931744687666964], 17);
